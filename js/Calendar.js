74db76f9815ed8c59a343b49f97945193da88643
hoonkong <hoonkong@gmail.com>
2014-08-10 00:42:52 -0700

    Added initial calendar generating

--- js/Calendar.js
+++ js/Calendar.js
@@ -1,3 +1,22 @@
 /**
  * Created by hoon on 8/10/14.
  */
+angular.module("calendarApp", [])
+    .controller("CalendarController", ["$scope", function ($scope) {
+
+        $scope.daysInCurrentMonth = (function () {
+            var days = [];
+            for (var i = 1; i <= 31; i++) {
+                days.push(i);
+            }
+
+            return days;
+        })();
+
+        $scope.getCurrentMonth = function () {
+            var monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+            var date = new Date();
+            var monthNumber = date.getMonth();
+            return monthNames[monthNumber];
+        }
+    }]);
